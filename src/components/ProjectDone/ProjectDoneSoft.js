82913b4aa1f19293429746c0b3da16b3783dd31d
Dobry <84011609+szmdm@users.noreply.github.com>
2022-07-15 12:20:00 +0200

    project soft next component

--- src/components/ProjectDone/ProjectDoneSoft.js
+++ src/components/ProjectDone/ProjectDoneSoft.js
@@ -14,14 +14,26 @@ import {
     ThirdContainerSoft,
     ThirdCSoftContent,
     ThirdCSoftText,
+    FourthContainerSoft,
+    FouthTitleBox,
+    FourthContentContainer,
+    FCCLeft,
+    FCCRight,
+    FCCTitle,
+    FCCColors,
+    FCCFonts,
+    FCCLeftColors,
+    FCCRightFonts,
 
 }
     from "./projectDoneSoftStyle"
 
 import PDSoftImg_01 from "../../assets/projectsDone/pDSoft/point_module02_view_01_circle_1.png"
-import PDSoftImg_02 from "../../assets/projectsDone/pDSoft/Hypo_allergenic_1.png"
 import PDSoftImg_03 from "../../assets/projectsDone/pDSoft/Image_Module_01.png"
 import PDSoftImg_14 from "../../assets/projectsDone/pDSoft/Image_Module_03_view_01.png"
+import PDSoftImg_15 from "../../assets/projectsDone/pDSoft/color_module03_01.png"
+import PDSoftImg_16 from "../../assets/projectsDone/pDSoft/color_module03_02.png"
+import PDSoftImg_17 from "../../assets/projectsDone/pDSoft/color_module03_03.png"
 
 
 
@@ -36,7 +48,7 @@ export const ProjectDoneSoft = () => {
 
     const DisplayComponent = () => {
         if (packshotStatus === 1) {
-            return <PackshotModulComponent setPackshotStatus={setPackshotStatus}/>
+            return <PackshotModulComponent setPackshotStatus={setPackshotStatus} />
         }
         if (packshotStatus === 2) {
             return <PackshotItemOneComponent />
@@ -49,36 +61,80 @@ export const ProjectDoneSoft = () => {
 
     return (
         <>
-                <ProjectContainerSoft>
-                    <FirstImageContainerSoft>
-                        <img src={PDSoftImg_03} alt="" />
-                        <FirstContainerSoft alignItems="center" flexDirection="column" >
-                            <FirstCSoftContentBox>
-                                <img src={PDSoftImg_01} alt="/" />
-                            </FirstCSoftContentBox>
-                            <FirstCSoftTitle>
-                                <img src={PDSoftImg_02} alt="/" />
-                            </FirstCSoftTitle>
-                        </FirstContainerSoft>
-                    </FirstImageContainerSoft>
-                </ProjectContainerSoft>
-                <ProjectContainerSoft>
-                    <SecondContainerSoft alignItems="center" flexDirection="row">
-                        {DisplayComponent()}
-                    </SecondContainerSoft>
-                </ProjectContainerSoft>
-                <ProjectContainerSoft>
-                    <ThirdContainerSoft>
+            <ProjectContainerSoft>
+                <FirstImageContainerSoft>
+                    <img src={PDSoftImg_03} alt="" />
+                    <FirstContainerSoft alignItems="center" flexDirection="column" >
+                        <FirstCSoftContentBox>
+                            <img src={PDSoftImg_01} alt="/" />
+                        </FirstCSoftContentBox>
+                        <FirstCSoftTitle>
+                            <div className='number'>hypo</div>
+                            <div className='under-number'>allergenic</div>
+                        </FirstCSoftTitle>
+                    </FirstContainerSoft>
+                </FirstImageContainerSoft>
+            </ProjectContainerSoft>
+            <ProjectContainerSoft>
+                <SecondContainerSoft alignItems="center" flexDirection="row">
+                    {DisplayComponent()}
+                </SecondContainerSoft>
+            </ProjectContainerSoft>
+            <ProjectContainerSoft>
+                <ThirdContainerSoft>
                     <img src={PDSoftImg_14} alt="" />
                     <div className='footerText'>read more</div>
-                        <ThirdCSoftContent>
-                            <ThirdCSoftText>
-                                <div className='number'>15</div>
-                                <div className='under-number'>SPF<br />sun protection</div>
-                            </ThirdCSoftText>
-                        </ThirdCSoftContent>
-                    </ThirdContainerSoft>
-                </ProjectContainerSoft>
+                    <ThirdCSoftContent>
+                        <ThirdCSoftText>
+                            <div className='number'>15</div>
+                            <div className='under-number'>SPF<br />sun protection</div>
+                        </ThirdCSoftText>
+                    </ThirdCSoftContent>
+                </ThirdContainerSoft>
+            </ProjectContainerSoft>
+            <ProjectContainerSoft>
+                <FourthContainerSoft>
+                    <FouthTitleBox>
+                        Colors & Fonts
+                    </FouthTitleBox>
+                    <FourthContentContainer>
+                        <FCCLeft>
+                            <FCCTitle>
+                                Colors
+                            </FCCTitle>
+                            <FCCColors>
+                                <FCCLeftColors>
+                                    <img src={PDSoftImg_15} alt="" />
+                                    <span>Light Grey<br />#f6f6f6<br /></span>
+                                </FCCLeftColors>
+                                <FCCLeftColors>
+                                    <img src={PDSoftImg_16} alt="" />
+                                    <span>Light Grey<br />#f6f6f6<br /></span>
+                                </FCCLeftColors>
+                                <FCCLeftColors>
+                                    <img src={PDSoftImg_17} alt="" />
+                                    <span>Light Grey<br />#f6f6f6<br /></span>
+                                </FCCLeftColors>
+                            </FCCColors>
+                        </FCCLeft>
+                        <FCCRight>
+                            <FCCTitle>
+                                Colors
+                            </FCCTitle>
+                            <FCCFonts>
+                                <FCCRightFonts fontFamily={"FuturaStd-Light"}>
+                                    <div className='big'>Futura PT</div>
+                                    <span>Light</span>
+                                </FCCRightFonts>
+                                <FCCRightFonts fontFamily={"FuturaStd-Book"}>
+                                    <div className='big'>Futura PT</div>
+                                    <span>Book</span>
+                                </FCCRightFonts>
+                            </FCCFonts>
+                        </FCCRight>
+                    </FourthContentContainer>
+                </FourthContainerSoft>
+            </ProjectContainerSoft>
         </>
     )
 }
--- src/components/ProjectDone/projectDoneSoftStyle.js
+++ src/components/ProjectDone/projectDoneSoftStyle.js
@@ -44,15 +44,50 @@ export const FirstContainerSoft = styled.div`
 `;
 
 export const FirstCSoftTitle = styled.span`
-    position: relative;
-    top: -45px;
-    margin-top: 0px;
-    text-align: center;
-    max-width: 172px;
-    height: auto;
 
-    @media screen and (max-width: ${mediaSize}) {
-        top: -20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    line-height: 2.25rem;
+    margin: 30px 0 25px 0;
+    position: relative;
+    top: -65px;
+
+      .number {
+          font-family: "FuturaStd-Light";
+          font-size: 9.375rem;
+          font-size: clamp(3rem, 5.625rem, 9.375rem);
+      }
+
+      .under-number {
+          font-family: "FuturaStd-Light";
+          font-size: 1.875rem;
+          font-size: clamp(1rem, 2.3rem, 2.5rem);
+          top: 1px;
+          text-align: center;
+          position: relative;
+          top: 50px;
+      }
+
+      @media screen and (max-width: ${mediaSize}) {
+        top: -70px;
+
+        .number {
+            font-size: 73px;
+            margin-bottom: 0px;
+            margin-left: 0;
+            letter-spacing: 0;
+        }
+        .under-number {
+            font-size: 20px;
+            line-height: 22px;
+            top: 30px;
+            margin-left: 0;
+        }
+    }
+
+    @media screen and (min-width: ${mediaSize}) and (max-width: ${laptop}) {
     }
 `;
 
@@ -76,7 +111,7 @@ export const SecondContainerSoft = styled.div`
   align-items: ${(props) => props.alignItems};
   justify-content: center;
   flex-direction: ${(props) => props.flexDirection};
-  margin-bottom: 15px;
+  margin-bottom: 1px;
   
     span {
         position:relative;
@@ -184,4 +219,103 @@ export const ThirdCSoftText = styled.div`
     }
 `;
 
+export const FourthContainerSoft = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    max-height: 572px;
+    width: 100%;
+    position: relative;
+    flex-direction: column;
+`
 
+export const FouthTitleBox = styled.div`
+    text-align: center;
+    margin: 85px 0 20px 0;
+    font-family: "FuturaStd-Light";
+    font-size: 1.875rem;
+    font-size: clamp(1rem, 1.875rem, 2.5rem);
+
+`
+
+export const FourthContentContainer = styled.div`
+    display: flex;
+    flex-grow: 1;
+    max-width: 600px;
+    height: 100%;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 120px;
+    gap: 70px;
+`
+
+export const FCCLeft = styled.div`
+    display: flex;
+    flex-grow: 1;
+    flex-direction: column;
+`
+
+export const FCCRight = styled.div`
+    display: flex;
+    flex-grow: 1;
+    flex-direction: column;
+`
+
+export const FCCTitle = styled.div`
+    text-align: center;
+    margin: 50px 0 30px 0;
+    font-family: "FuturaStd-Book";
+    font-size: 1rem;
+    font-size: clamp(0.7rem, 1rem, 1.5rem);
+`
+
+export const FCCColors = styled.div`
+    display: flex;
+    flex-direction: row;
+    gap: 15px;
+`
+
+export const FCCLeftColors = styled.div`
+    display: flex;
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+    img {
+        margin: 0px 0 20px 0;
+    }
+    span {
+        text-align: center;
+        font-family: "FuturaStd-Light";
+        font-size: 1rem;
+        font-size: clamp(0.7rem, 1rem, 1.5rem);
+    }
+`
+export const FCCFonts = styled.div`
+    display: flex;
+    flex-direction: row;
+    gap: 15px;
+    margin: 48px 0 101px 0;
+`
+export const FCCRightFonts = styled.div`
+    display: flex;
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    text-align: center;
+    min-width: 120px;
+    margin: 0px 20px 0px 20px;
+
+    .big {
+        font-family: ${(props) => props.fontFamily};
+        font-size: 1.875rem;
+        font-size: clamp(1rem, 1.875rem, 2.5rem);
+    }
+    span {
+        font-family: ${(props) => props.fontFamily};
+        font-size: 1rem;
+        font-size: clamp(0.7rem, 1rem, 1.5rem);
+    }
+`
